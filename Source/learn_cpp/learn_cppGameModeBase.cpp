d46da525fd06ba958c74ef567ab149648d99df60
DaVinci_013 <2843749493@qq.com>
2022-12-25 10:09:42 +0800

    上传新项目

---
+++ Source/learn_cpp/learn_cppGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "learn_cppGameModeBase.h"
+
---
+++ Source/learn_cpp/learn_cppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "learn_cppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARN_CPP_API Alearn_cppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
